e331feb8e0b98f88e9a4d7069bfab40f7b34fb7e
Sebastian Kuligowski <sebastian@kuligowski.pl>
2016-10-05 01:21:35 +0200

    Cli setup, options refactoring

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var logger = require('./lib/logger'),
-	path = require('path');
+var log = require('./lib/logger');
+var path = require('path');
 
 function createTempOutputDir() {
-	var temp = require('temp').track(),
-		tempDir = temp.mkdirSync('docsplayer');
+	var temp = require('temp').track();
+	var tempDir = temp.mkdirSync('docsplayer');
 
 	process.on('SIGINT', function() {
 		temp.cleanupSync();			
@@ -13,27 +13,47 @@ function createTempOutputDir() {
 	return tempDir;
 }
 
-function startDocs(docsSrcPattern, opts) {
-	var docsServer = require('./lib/docs-server'),
-		docsBuilder = require('./lib/docs-builder'),
-		options = opts || {},
-		docsDestDir = options.docsDestDir || createTempOutputDir();
+function startDocs(src, opts) {
 
-	logger.setDebug(options.debug || false);
-	var builderOptions = {
-		watch: options.watch || false,
-		cwd: options.cwd,
-		ignored: options.ignored || /([\/\\]\.|node_modules)/
+	opts = opts || {};
+	
+	var options = {
+		port: opts.port || 8000,
+		debug: opts.debug || false,
+		ignored: opts.ignored || /([\/\\]\.|node_modules)/,
+		src: src || path.join(process.cwd(), '**/*.md'),
+		docsDestDir: opts.docsDestDir || createTempOutputDir(),
+		watch: opts.watch || false,
 	};
-	docsBuilder.build(docsSrcPattern, docsDestDir, builderOptions, function() {
+	
+	var docsServer = require('./lib/docs-server');
+	var docsBuilder = require('./lib/docs-builder');	
+
+	log.setDebug(options.debug || false);
+	docsBuilder.build(options.src, options.docsDestDir, options, function() {
 		docsServer.start({
 			playerSrc: path.join(__dirname, 'player'),
-			docsSrc: docsDestDir,
+			docsSrc: options.docsDestDir,
 			port: options.port || 8000
 		});		
 	});
 }
 
+function createTheme(themeName) {
+	log.info('Creating theme ... ' + themeName);
+}
+
+function runCli() {
+	var argv = require('yargs').argv;
+	
+	if (argv.createTheme) {
+		return createTheme(argv.createTheme);
+	}	
+
+	startDocs(argv.src, argv);
+}
+
 module.exports = {
-	start: startDocs
+	start: startDocs,
+	cli: runCli
 };
